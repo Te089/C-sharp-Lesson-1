de80e1d45e5880fa32e505f948ff1c80e057ae3d
Sergey <rsa89@bk.ru>
2022-08-30 21:45:53 +0300

    задача8

---
+++ Задача 8/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int value = Prompt("Введите число > ");
+for(int index = 2; index < value+1; index = index + 2 )
+{
+    System.Console.Write($" {index},");
+}
