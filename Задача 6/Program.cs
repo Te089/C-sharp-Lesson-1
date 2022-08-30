848944e7a73f8e7fcecea6962fcb09e7cf0d3f61
Sergey <rsa89@bk.ru>
2022-08-30 21:44:32 +0300

    задача6

---
+++ Задача 6/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int firstValue = Prompt("Введите первое число > ");
+if (firstValue % 2 == 0)
+{
+    System.Console.WriteLine("-> да, чётное");
+} else
+{
+    System.Console.WriteLine("-> нет, не чётное");
+}
